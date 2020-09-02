ee276f208ab2cdc6a84b502d9f1016ee2e83ebd2
haolp <haolp@cn.ibm.com>
2020-09-02 16:33:26 +0800

    correct ubuntu fcp device file name in ubuntu20

--- zvmsdk/dist.py
+++ zvmsdk/dist.py
@@ -1928,6 +1928,16 @@ class ubuntu20(ubuntu):
         return var_source_device
 
 
+    def _get_source_devices(self, fcp, target_lun):
+        """ubuntu"""
+        device = '0.0.%s' % fcp
+        target_lun = self._format_lun(target_lun)
+        var_source_device = ('SourceDevices=(`ls /dev/disk/by-path/ | '
+                             'grep "ccw-%s-fc-.*-lun-%s"`)\n'
+                             % (device, target_lun))
+        return var_source_device
+
+
 class LinuxDistManager(object):
     def get_linux_dist(self, os_version):
         distro, release = self.parse_dist(os_version)
